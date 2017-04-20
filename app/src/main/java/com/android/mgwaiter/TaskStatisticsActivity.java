b162e8fe0b70dac5f3e14550ad5ec6d7fb4fd577
huyanan <1103995626@qq.com>
2017-04-20 18:00:54 +0800

    UI界面调整

--- app/src/main/java/com/android/mgwaiter/TaskStatisticsActivity.java
+++ app/src/main/java/com/android/mgwaiter/TaskStatisticsActivity.java
@@ -25,13 +25,15 @@ import java.util.Map;
 
 public class TaskStatisticsActivity extends AppCompatActivity {
 //    private TextView rr, rrr;//首页返回按钮和日历按钮的  （占位）
-//    private StatusTitle statusTitle;
-////    TaskAdapter adapter;
-//
-//
-//    List<String> parent = null;//列表头
-//    Map<String, List<String>> map = null;//列表详情
-//    private ViewPager viewpager;
+    private TextView tv_date,tv_order_task,tv_miss_task,tv_general_assignment,tv_order_rate;
+    private StatusTitle statusTitle;
+//    TaskAdapter adapter;
+
+
+    List<String> parent = null;//列表头
+    Map<String, List<String>> map = null;//列表详情
+    private ViewPager viewpager;
+    Fragment finishFragment,abolishFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,51 +42,49 @@ public class TaskStatisticsActivity extends AppCompatActivity {
 //        inView();
 //        initStatusBar();
     }
-//
-//    //初始化控件
-//    private void inView() {
-//        viewpager = (ViewPager) findViewById(R.id.viewpager);
-//
-//    }
-//
-//    private void initStatusBar() {
-//        statusTitle = (StatusTitle) findViewById(R.id.status_title);
-//        initData();
-//        FragmentManager fragmentManager = getSupportFragmentManager();
+
+    //初始化控件
+    private void inView() {
+        viewpager = (ViewPager) findViewById(R.id.viewpager);//日期
+        tv_order_task = (TextView) findViewById(R.id.tv_order_task);//接单任务总计
+        tv_miss_task = (TextView) findViewById(R.id.tv_miss_task);//未接单任务总计
+        tv_general_assignment = (TextView) findViewById(R.id.tv_general_assignment);//推送总计
+        tv_order_rate = (TextView) findViewById(R.id.tv_order_rate);//接单率
+
+    }
+
+    private void initStatusBar() {
+        statusTitle = (StatusTitle) findViewById(R.id.status_title);
+        initData();
+        FragmentManager fragmentManager = getSupportFragmentManager();
 //        adapter = new TaskAdapter(fragmentManager);
-//
-//
-////        statusTitle.setViewPager(viewpager);
-//        statusTitle.setOnTitleListener(new StatusTitle.OnTitleListener() {
-//            @Override
-//            //已完成点击事件
-//            public void onCenterLeft() {
-//                viewpager.setCurrentItem(0);
-//            }
-//
-//            //已取消点击事件
-//            @Override
-//            public void onCenterRight() {
-//                viewpager.setCurrentItem(1);
-//            }
-//        });
-//
+//        statusTitle.setViewPager(viewpager);
+        statusTitle.setOnTitleListener(new StatusTitle.OnTitleListener() {
+            @Override
+            //已完成点击事件
+            public void onCenterLeft() {
+                viewpager.setCurrentItem(0);
+            }
+            //已取消点击事件
+            @Override
+            public void onCenterRight() {
+                viewpager.setCurrentItem(1);
+            }
+        });
 //        viewpager.setAdapter(adapter);
-//    }
-//
-//    private List<Fragment> list_fragment;
-//
-//    //初始化按钮数据
-//    private void initData() {
-//        list_fragment = new ArrayList<>();
-//        list_fragment.add(new FinishFragment());
-//        list_fragment.add(new AbolishFragment());
-//    }
-//
-//
-//    boolean[] fragmentsUpdateFlag = {false, false};
-//
-//
+    }
+
+    private List<Fragment> list_fragment;
+
+    //初始化按钮数据
+    private void initData() {
+        list_fragment = new ArrayList<>();
+        list_fragment.add(new FinishFragment());
+        list_fragment.add(new AbolishFragment());
+    }
+
+
+    boolean[] fragmentsUpdateFlag = {false, false};
 //
 //    public class TaskAdapter extends FragmentPagerAdapter {
 //        FragmentManager fm;
@@ -93,60 +93,54 @@ public class TaskStatisticsActivity extends AppCompatActivity {
 //            super(fm);
 //            this.fm = fm;
 //        }
-//
+
 //        @Override
 //        public Fragment getItem(int position) {
-//            return null;
-//        }
+//            return list_fragment.get(position);
+//            switch (position) {
+//                case 0:
+//                    if (finishFragment == null) {
+//                        finishFragment = new FinishFragment();
+//                    }
+//                    return finishFragment;
+//                case 1:
+//                    if (abolishFragment == null) {
+//                        abolishFragment = new AbolishFragment();
+//                    }
+//                    return abolishFragment;
+//                default:
+//                    return null;
 //
+//            }
+//        }
 //        @Override
 //        public int getCount() {
 //                return list_fragment.size();
 //        }
 //    }
+
+
+//    public Object instantiateItem(ViewGroup container, int position) {
+//        //得到缓存的fragment
+//        Fragment fragment = (Fragment) super.instantiateItem(container,position);
+//        //得到tag
+//        String fragmentTag = fragment.getTag();
+//        if (fragmentsUpdateFlag[position % fragmentsUpdateFlag.length]) {
+//            //如果这个fragment需要更新
+//            FragmentTransaction ft = fragment.beginTransaction();
+//            //移除旧的fragment
+//            ft.remove(fragment);
+//            //换成新的fragment
+//            fragment = list_fragment.get(position % list_fragment.size());
+//            //添加新fragment时必须用前面获得的tag ❶
+//            ft.add(container.getId(), fragment, fragmentTag);
+//            ft.attach(fragment);
+//            ft.commit();
+//            //复位更新标志
+//            fragmentsUpdateFlag[position % fragmentsUpdateFlag.length] = false;
 //
-//
-////    public Object instantiateItem(ViewGroup container, int position) {
-////        //得到缓存的fragment
-////        Fragment fragment = (Fragment) super.instantiateItem(container,position);
-////        //得到tag
-////        String fragmentTag = fragment.getTag();
-////        if (fragmentsUpdateFlag[position % fragmentsUpdateFlag.length]) {
-////            //如果这个fragment需要更新
-////            FragmentTransaction ft = fm.beginTransaction();
-////            //移除旧的fragment
-////            ft.remove(fragment);
-////            //换成新的fragment
-////            fragment = list_fragment.get(position % list_fragment.size());
-////            //添加新fragment时必须用前面获得的tag ❶
-////            ft.add(container.getId(), fragment, fragmentTag);
-////            ft.attach(fragment);
-////            ft.commit();
-////            //复位更新标志
-////            fragmentsUpdateFlag[position % fragmentsUpdateFlag.length] = false;
-////
-////        }
-////        return fragment;
-////
-////    }
-//
-//    public Fragment getItem(int position) {
-//        return list_fragment.get(position);
-////            switch (position) {
-////                case 0:
-////                    if (finishFragment == null) {
-////                        finishFragment = new FinishFragment();
-////                    }
-////                    return finishFragment;
-////                case 1:
-////                    if (abolishFragment == null) {
-////                        abolishFragment = new AbolishFragment();
-////                    }
-////                    return abolishFragment;
-////                default:
-////                    return null;
-////
-////            }
+//        }
+//        return fragment;
 //    }
 
 }
