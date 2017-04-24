ad2b51eec7417f3a646feedee888bfa5481ecd1c
humorboy <humorboy@126.com>
2017-04-24 13:55:44 +0800

    集成阿里百川即时通讯SDK

--- app/src/main/java/com/android/mgwaiter/application/MgApplication.java
+++ app/src/main/java/com/android/mgwaiter/application/MgApplication.java
@@ -2,6 +2,8 @@ package com.android.mgwaiter.application;
 
 import android.app.Application;
 
+import com.alibaba.mobileim.YWAPI;
+import com.alibaba.wxlib.util.SysUtil;
 import com.fengmap.android.FMMapSDK;
 import com.fengmap.drpeng.common.ResourcesUtils;
 
@@ -11,6 +13,7 @@ import com.fengmap.drpeng.common.ResourcesUtils;
 
 public class MgApplication extends Application {
     static MgApplication instance;
+    final String APP_KEY = "23758144";
 
     public static MgApplication getInstance() {
         if (instance == null)
@@ -23,5 +26,15 @@ public class MgApplication extends Application {
         FMMapSDK.init(this, ResourcesUtils.getSDPath() + "/fm_drpeng");
         super.onCreate();
         instance=this;
+        //必须首先执行这部分代码, 如果在":TCMSSevice"进程中，无需进行云旺（OpenIM）和app业务的初始化，以节省内存;
+        SysUtil.setApplication(this);
+        if(SysUtil.isTCMSServiceProcess(this)){
+            return;
+        }
+        //第一个参数是Application Context
+        //这里的APP_KEY即应用创建时申请的APP_KEY，同时初始化必须是在主进程中
+        if(SysUtil.isMainProcess()){
+            YWAPI.init(instance, APP_KEY);
+        }
     }
 }
